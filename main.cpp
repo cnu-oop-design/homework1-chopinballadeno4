2c2bb9ef6d7cb50eccc76a0badf0a25d5c87d14f
chopinballadeno4 <qo98333@naver.com>
2026-09-05 14:00:11 +0900

    202650500 배성민

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번: 202650500
+// 이름: 배성민
 
 int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
@@ -12,7 +12,7 @@ int main(int argc, const char *argv[]) {
     //   C++ is ready.
 
     std::cout << "Hello, OOP!\n";
-    std::cout << "C is ready.\n";
+    std::cout << "C++ is ready.\n";
 
     return 0;
 }
